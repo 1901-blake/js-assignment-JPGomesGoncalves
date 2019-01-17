4da2592af59316d7663e6db784b62b3478d9358e
JPGomesGoncalves <JPGomesGoncalves@student.fullsail.edu>
2019-01-16 19:18:28 -0500

    replaced the if statments in the shape with switch as per instructions

--- problems/08-shapes/08-shapes.js
+++ problems/08-shapes/08-shapes.js
@@ -22,51 +22,52 @@ Example for printShape("Diamond", 5, "*");
 */
 function printShape(shape, height, character) {
 
-  if(shape.toLowerCase() == 'square'){
-    let lengtharr = [height];
-    for (let row = 0; row < height; row++) {
-      for (let col = 0; col < height; col++) {
-
-        lengtharr[row,col] = character;
-      }
-      console.log(lengtharr); 
+  switch (shape.toLowerCase()) {
+    case 'square':{
+       let lengtharr = [height];
+        for (let row = 0; row < height; row++) {
+          for (let col = 0; col < height; col++) {
+            lengtharr[row,col] = character;
+          }
+          console.log(lengtharr); 
+        }
+    break;
     }
-  }
-  else if(shape.toLowerCase() == 'triangle'){
-    let lengtharr = [height];
-    let colinc = 1;
+    case 'triangle':{
+      let lengtharr = [height];
+      let colinc = 1;
 
-    for (let row = 0; row < height; row++) {
-      if (colinc <= height) {
-        for (let col = 0; col < colinc; col++) {
-
+      for (let row = 0; row < height; row++) {
+        if (colinc <= height) {
+          for (let col = 0; col < colinc; col++) {
           lengtharr[row,col] = character;
+          } 
         }
+        colinc++;
+        console.log(lengtharr); 
       }
-      colinc++;
-      console.log(lengtharr); 
+    break;
     }
-  }
-  else if(shape.toLowerCase() == 'diamond'){
-    let lengtharr = [];
-    let pattern = 0;// iadjusts the characters indexes in each row
-    let patterncounter = 0;// keep track of how many character are on each side
-    let decrease = false;// halfway mark indicator
-    let thatextragap = false;// offsets the pattern so that it fixes every even number with the same patter after the halfway mark is reached
+    case 'diamond':{
+      let lengtharr = [];
+      let pattern = 0;// iadjusts the characters indexes in each row
+      let patterncounter = 0;// keep track of how many character are on each side
+      let decrease = false;// halfway mark indicator
+      let thatextragap = false;// offsets the pattern so that it fixes every even number with the same patter after the halfway mark is reached
     
-    for (let row = 0; row < height; row++) {
-      for (let col = 0; col < height; col++) 
-      {
-        if (col < Math.trunc(height/2) - pattern || col > Math.trunc(height/2) + pattern) {
-          lengtharr[row,col] = '_';
-        }
+      for (let row = 0; row < height; row++) {
+        for (let col = 0; col < height; col++) {
+          if (col < Math.trunc(height/2) - pattern || col > Math.trunc(height/2) + pattern) {
+            lengtharr[row,col] = '_';
+          }
+
           if (col === Math.trunc(height/2)) {
             lengtharr[row,col] = character;
             lengtharr[row,col + pattern] = character;
             lengtharr[row,col - pattern] = character;
           }
 
-      }
+        }
 
         if (patterncounter + 2 >= height) {
           decrease = true;
@@ -79,12 +80,15 @@ function printShape(shape, height, character) {
             thatextragap = true;
           }
         } else{
-          pattern++;
-          patterncounter += 2;
-        }
-
+            pattern++;
+            patterncounter += 2;
+          }
       console.log(lengtharr); 
+      }
+    break;
     }
+    default:
+      break;
   }
 }
 
